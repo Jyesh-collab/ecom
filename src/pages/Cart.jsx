e425cb5720bb1cf6775212ac93b43cf8017205b0
Jyesh-collab <jyesh.ranjan@qualitrix.com>
2026-04-19 19:36:43 +0530

    test: update checkout button text for AI pipeline testing

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -84,7 +84,7 @@ function Cart({ cart, setCart }) {
                 Continue shopping
               </Button>
               <Button variant="contained" onClick={handleCheckout}>
-                Proceed to Checkout
+                Get the product
               </Button>
             </Stack>
           </Box>
