a7183bab95132375de851536cf69577854de77a0
Jyesh-collab <jyesh.ranjan@qualitrix.com>
2026-06-04 19:06:00 +0530

    cart logic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -124,7 +124,7 @@ export default function ProductCard({ product, addToCart }) {
             addToCart(product);
           }}
         >
-          Put Uday In cart
+          Add to Cart
         </Button>
         <Tooltip title="See full specs" arrow>
           <Button
